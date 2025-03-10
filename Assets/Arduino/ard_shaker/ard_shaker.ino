6e4bf109c8150f4e73e1df5c99d7658066e4b986
OtoSh8 <2201105@student.toa.edu.my>
2025-03-10 19:02:48 +0800

    new shaker

--- Assets/Arduino/ard_shaker/ard_shaker.ino
+++ Assets/Arduino/ard_shaker/ard_shaker.ino
@@ -3,15 +3,27 @@ bool down = false;
 void setup() {
   Serial.begin(9600);
   pinMode(12, INPUT_PULLUP);
+  pinMode(11, OUTPUT);
+
 }
 
 void loop() {
   if(digitalRead(12) == HIGH && down == false){
-    Serial.println("1");
     down = true;
   }
   else if(digitalRead(12) == LOW && down == true){
     Serial.println("0");
     down = false;
   }
+
+  if(digitalRead(12) == HIGH){
+      digitalWrite(11, LOW );
+
+  }
+  else{
+  digitalWrite(11, HIGH);
+
+  }
+
+  delay(100);
 }
